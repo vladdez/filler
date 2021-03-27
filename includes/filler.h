3552d84692440639158c550bb78db60e45782985
Kirstie Ysgramor <kysgramo@at-e5.msk.21-school.ru>
2021-03-27 17:25:38 +0300

    g

--- includes/filler.h
+++ includes/filler.h
@@ -4,4 +4,14 @@
 
 # include "libft.h"
 
+typedef struct	s_filler
+{
+	char		player_symbol;
+	char		enemy_symbol;
+	t_plateau	*plateau;
+	t_piece		*piece;
+	int			y;
+	int			x;
+}				t_filler;
+
 #endif
--- src/filler.c
+++ src/filler.c
@@ -2,5 +2,6 @@
 
 int main(int ac, char **av)
 {
+    t_filler	*filler
     return(1);
 }
